8b8a852784ba19760c57b2853bba4272d45efbcf
7thCore <34578834+7thCore@users.noreply.github.com>
2019-08-18 14:31:28 +0200

    Update hlnsrv-script.bash

--- hlnsrv-script.bash
+++ hlnsrv-script.bash
@@ -4,7 +4,7 @@
 #If you do not know what any of these settings are you are better off leaving them alone. One thing might brake the other if you fiddle around with it.
 #Leave this variable alone, it is tied in with the systemd service file so it changes accordingly by it.
 SCRIPT_ENABLED="0"
-export VERSION="201908181400"
+export VERSION="201908181432"
 
 #Basics
 export NAME="IsRSrv" #Name of the screen
