8900fa95434b187b036c8e29f7d12fa6a2796360
7thCore <the7thcore@gmail.com>
2020-01-30 16:47:12 +0100

    - added check for executing functions/arguments to only allow -install and -install_packages functions to be allowed to run with sudo or as root\n- added check for executing functions/arguments to allow notification sending functions to run along side another instance of the script\n- added warning to readme and the script for the update script from github function for security reasons

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # hlnsrv-script
 Bash script for running Hellion on a linux server
 
+-------------------------
+
 **Required packages**
 
 - xvfb
 
 - rsync
 
-- tmux
+- tmux (minimum version: 2.9a)
 
 - wine
 
@@ -24,6 +26,8 @@ Bash script for running Hellion on a linux server
 - postfix (optional for email notifications)
 
 - zip (optional but required if using the email feature)
+
+-------------------------
 
 **Features:**
 
@@ -67,49 +71,100 @@ Bash script for running Hellion on a linux server
 
 - supports multiple discord webhooks
 
-**Instructions:**
+-------------------------
+
+# WARNING
+
+- Script updates from GitHub: These may include malicious code to steal any info the script uses to work, like email credentials and discord webhooks.
+Now I'm not saying that I'm that kind of person that would do that but:
+
+**IF YOU DON'T TRUST ME, LEAVE THIS OFF FOR SECURITY REASONS!**
+
+-------------------------
+
+# What does this script do?
+
+This script creates a new non-sudo enabled user and installes the game in a folder called server in the user's home folder. It also installes systemd services for starting and shutting down the game server when the computer starts up, shuts down or reboots and also installs systemd timers so the script is executed on timed intervals (every 15 minutes) to do it's work like automatic game updates and backups. It will also create a config file in the script folder that will save the configuration you defined between the installation process. The reason for user creation is to limit the script's privliges so it CAN NOT be used with sudo when handeling the game server. Sudo is only needed for installing the script (for user creation) and installing packages (it the script supports the distro you are running).
+
+-------------------------
+
+# Supported distros
+
+- Arch Linux
+
+- Ubuntu 19.10
+
+I will add suport for the next Ubuntu LTS version when it's released.
+
+The script can, in theory run on any systemd-enabled distro. So if you are not using Arch Linux or Ubuntu 19.10 I suggest you check your distro's wiki on how to install the required packages. The script can, in theory install packages for any Ubuntu version, but the repositories for old versions of Ubuntu might have outdated packages and cause problems.
+
+-------------------------
+
+# Installation
+
+-------------------------
+
+**Download the script:**
 
 Log in to your server with ssh and execute:
 
-```git clone https://github.com/7thCore/hlnsrv-script```
+`git clone https://github.com/7thCore/hlnsrv-script`
 
 Make it executable:
 
-```chmod +x ./hlnsrv-script.bash```
+`chmod +x ./hlnsrv-script.bash`
 
-If you plan on using a ramdisk to run your server from, the script will give you that option.
+-------------------------
 
-Now for the installation.
+**Installation:**
 
 If you wish you can have the script install the required packages with (Only for Arch Linux & Ubuntu 19.10):
 
-```sudo ./hlnsrv-script.bash -install_packages```
+`sudo ./hlnsrv-script.bash -install_packages`
 
 After that run the script with root permitions like so (necessary for user creation):
 
-```sudo ./hlnsrv-script.bash -install```
+`sudo ./hlnsrv-script.bash -install`
 
-The script will create a new non-sudo enabled user from wich the game server will run. If you want to have multiple game servers on the same ma
-chine just run the script multiple times but with a diffrent username inputted to the script.
+You can also install bash aliases to make your life easier by logging in to the newly created user and executing the script with the following command:
 
-After the installation finishes you can reboot the operating system and the service files will start the game server automaticly on boot.
+`./hlnsrv-script.bash -install_aliases`
 
-You can also install bash aliases to make your life easier with the following command:
+After the installation finishes you can log in to the newly created user and fine tune your game configuration and then reboot the operating system and the service files will start the game server automaticly on boot.
 
-```./hlnsrv-script.bash -install_aliases```
+-------------------------
 
-After that relog.
+# Available commands:
 
-Any other script commands are available with:
+| Command | Description |
+| ------- | ----------- |
+| `-help` | Prints a list of commands and their description |
+| `-start` | Start the server |
+| `-stop` | Stop the server |
+| `-restart` | Restart the server |
+| `-sync` | Sync from tmpfs to hdd/ssd |
+| `-backup` | Backup files, if server running or not |
+| `-autobackup` | Automaticly backup files when server running |
+| `-deloldbackup` | Delete old backups |
+| `-delete_save` | Delete the server's save game with the option for deleting/keeping the GameServer.ini file |
+| `-deloldsavefiles` | Delete the server's save game (leaves the latest number files specefied in the script conf file |
+| `-change_branch` | Changes the game branch in use by the server (public,experimental,legacy and so on) |
+| `-install_aliases` | Installs .bashrc aliases for easy access to the server tmux session |
+| `-rebuild_tmux_config` | Reinstalls the tmux configuration file from the script. Usefull if any tmux configuration updates occoured. |
+| `-rebuild_services` | Reinstalls the systemd services from the script. Usefull if any service updates occoured |
+| `-rebuild_prefix` | Reinstalls the wine prefix. Usefull if any wine prefix updates occoured |
+| `-disable_services` | Disables all services. The server and the script will not start up on boot anymore. |
+| `-enable_services` | Enables all services dependant on the configuration file of the script |
+| `-reload_services` | Reloads all services, dependant on the configuration file |
+| `-update` | Update the server, if the server is running it wil save it, shut it down, update it and restart it |
+| `-update_script` | Check github for script updates and update if newer version available |
+| `-update_script_force` | Get latest script from github and install it no matter what the version |
+| `-status` | Display status of server |
+| `-install` | Installs all the needed files for the script to run, the wine prefix and the game |
+| `-install_packages` | Installs all the needed packages (Supports only Arch linux & Ubuntu 19.10 and onward) |
 
-```./hlnsrv-script.bash -help```
-
-That should be it.
+-------------------------
 
 **Known issues are:**
 
--Wine version 4.12 and later are fubar. Use 4.11 or lower.
-
 -The winetricks package in ubuntu is outdated. Follow this guide to install the latest winetricks: https://wiki.winehq.org/Winetricks (needed for dotnet472)
-
--if for some reason systemd reports the service failed when it stops, don't worry about it, the server session shuts down gracefully. (This should be solved)
--- hlnsrv-script.bash
+++ hlnsrv-script.bash
@@ -2,16 +2,21 @@
 
 #Hellion server script by 7thCore
 #If you do not know what any of these settings are you are better off leaving them alone. One thing might brake the other if you fiddle around with it.
-export VERSION="202001211631"
+export VERSION="202001301608"
 
 #Basics
 export NAME="HlnSrv" #Name of the tmux session
 if [ "$EUID" -ne "0" ]; then #Check if script executed as root and asign the username for the installation process, otherwise use the executing user
 	USER="$(whoami)"
 else
-	echo "WARNING: Installation mode"
-	read -p "Please enter username (leave empty for hellion):" USER #Enter desired username that will be used when creating the new user
-	USER=${USER:=hellion} #If no username was given, use default
+	if [ "$1" == "-install" ] || [ "$1" == "-install_packages" ]; then
+		echo "WARNING: Installation mode"
+		read -p "Please enter username (leave empty for arma):" USER #Enter desired username that will be used when creating the new user
+		USER=${USER:=hellion} #If no username was given, use default
+	else
+		echo "Error: This script, once installed, is meant to be used by the user it created and should not under any circumstances be used with sudo or by the root user for the $1 function. Only -install and -install_packages work with sudo/root. Log in to your created user (default: arma) with sudo -i -u arma and execute your script without root from the coresponding scripts folder."
+		exit 1
+	fi
 fi
 
 #Server configuration
@@ -1206,13 +1211,13 @@ script_install() {
 	read -p "Press any key to continue" -n 1 -s -r
 	echo ""
 	read -p "Enter password for user $USER: " USER_PASS
-	echo ""
-	read -p "Enable RamDisk (y/n): " TMPFS
-	echo ""
 	
 	sudo useradd -m -g users -s /bin/bash $USER
 	echo -en "$USER_PASS\n$USER_PASS\n" | sudo passwd $USER
 	
+	echo ""
+	read -p "Enable RamDisk (y/n): " TMPFS
+	echo ""
 	if [[ "$TMPFS" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 		TMPFS_ENABLE="1"
 		read -p "Do you already have a ramdisk mounted at /mnt/tmpfs? (y/n): " TMPFS_PRESENT
@@ -1230,7 +1235,6 @@ script_install() {
 	echo ""
 	read -p "Enable beta branch? Used for experimental and legacy versions. (y/n): " SET_BETA_BRANCH_STATE
 	echo ""
-	
 	if [[ "$SET_BETA_BRANCH_STATE" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 		BETA_BRANCH_ENABLED="1"
 		echo "Look up beta branch names at https://steamdb.info/app/$APPID/depots/"
@@ -1242,7 +1246,11 @@ script_install() {
 	fi
 	
 	echo ""
+	echo "WARNING: script updates from github may include malicious code to steal any info the script uses to work, like email accound and password."
+	echo "Not saying i'm that kind of person that would do that but:"
+	echo "IF YOU DON'T TRUST ME, LEAVE THIS OFF FOR SECURITY REASONS!"
 	read -p "Enable automatic updates for the script from github? (y/n): " SCRIPT_UPDATE_CONFIG
+	SCRIPT_UPDATE_CONFIG=${SCRIPT_UPDATE_CONFIG:=n}
 	if [[ "$SCRIPT_UPDATE_CONFIG" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 		SCRIPT_UPDATE_ENABLED="1"
 	else
@@ -1481,9 +1489,11 @@ script_install() {
 }
 
 #Do not allow for another instance of this script to run to prevent data loss
-if [[ $(pidof -o %PPID -x $0) -gt "0" ]]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S") [$NAME] [INFO] Another instance of this script is already running. Exiting to prevent data loss."
-	exit 0
+if [ "$1" != "-send_notification_start_initialized" ] || [ "$1" != "-send_notification_start_complete" ] || [ "$1" != "-send_notification_stop_initialized" ] || [ "$1" != "-send_notification_stop_complete" ] || [ "$1" != "-send_notification_crash" ]; then
+	if [[ $(pidof -o %PPID -x $0) -gt "0" ]]; then
+		echo "$(date +"%Y-%m-%d %H:%M:%S") [$NAME] [INFO] Another instance of this script is already running. Exiting to prevent data loss."
+		exit 0
+	fi
 fi
 
 if [ "$EUID" -ne "0" ] && [ -f "$SCRIPT_DIR/$SERVICE_NAME-config.conf" ]; then #Check if script executed as root, if not generate missing config fields
@@ -1508,29 +1518,29 @@ case "$1" in
 		echo -e "${CYAN}$NAME server script by 7thCore${NC}"
 		echo "Version: $VERSION"
 		echo ""
-		echo -e "${GREEN}start ${RED}- ${GREEN}Start the server${NC}"
-		echo -e "${GREEN}stop ${RED}- ${GREEN}Stop the server${NC}"
-		echo -e "${GREEN}restart ${RED}- ${GREEN}Restart the server${NC}"
-		echo -e "${GREEN}sync ${RED}- ${GREEN}Sync from tmpfs to hdd/ssd${NC}"
-		echo -e "${GREEN}backup ${RED}- ${GREEN}Backup files, if server running or not.${NC}"
-		echo -e "${GREEN}autobackup ${RED}- ${GREEN}Automaticly backup files when server running${NC}"
-		echo -e "${GREEN}deloldbackup ${RED}- ${GREEN}Delete old backups${NC}"
-		echo -e "${GREEN}delete_save ${RED}- ${GREEN}Delete the server's save game with the option for deleting/keeping the server.json and SSK.txt files.${NC}"
-		echo -e "${GREEN}deloldsavefiles ${RED}- ${GREEN}Delete the server's save game (leaves the latest number files specefied in the script conf file.${NC}"
-		echo -e "${GREEN}change_branch ${RED}- ${GREEN}Changes the game branch in use by the server (public,experimental,legacy and so on).${NC}"
-		echo -e "${GREEN}install_aliases ${RED}- ${GREEN}Installs .bashrc aliases for easy access to the server tmux session.${NC}"
-		echo -e "${GREEN}rebuild_tmux_config ${RED}- ${GREEN}Reinstalls the tmux configuration file from the script. Usefull if any tmux configuration updates occoured.${NC}"
-		echo -e "${GREEN}rebuild_services ${RED}- ${GREEN}Reinstalls the systemd services from the script. Usefull if any service updates occoured.${NC}"
-		echo -e "${GREEN}rebuild_prefix ${RED}- ${GREEN}Reinstalls the wine prefix. Usefull if any wine prefix updates occoured.${NC}"
-		echo -e "${GREEN}disable_services ${RED}- ${GREEN}Disables all services. The server and the script will not start up on boot anymore.${NC}"
-		echo -e "${GREEN}enable_services ${RED}- ${GREEN}Enables all services dependant on the configuration file of the script.${NC}"
-		echo -e "${GREEN}reload_services ${RED}- ${GREEN}Reloads all services, dependant on the configuration file.${NC}"
-		echo -e "${GREEN}update ${RED}- ${GREEN}Update the server, if the server is running it wil save it, shut it down, update it and restart it.${NC}"
-		echo -e "${GREEN}update_script ${RED}- ${GREEN}Check github for script updates and update if newer version available.${NC}"
-		echo -e "${GREEN}update_script_force ${RED}- ${GREEN}Get latest script from github and install it no matter what the version.${NC}"
-		echo -e "${GREEN}status ${RED}- ${GREEN}Display status of server${NC}"
-		echo -e "${GREEN}install ${RED}- ${GREEN}Installs all the needed files for the script to run, the wine prefix and the game.${NC}"
-		echo -e "${GREEN}install_packages ${RED}- ${GREEN}Installs all the needed packages (Supports only Arch linux & Ubuntu 19.10 and onward)"
+		echo -e "${GREEN}-start ${RED}- ${GREEN}Start the server${NC}"
+		echo -e "${GREEN}-stop ${RED}- ${GREEN}Stop the server${NC}"
+		echo -e "${GREEN}-restart ${RED}- ${GREEN}Restart the server${NC}"
+		echo -e "${GREEN}-sync ${RED}- ${GREEN}Sync from tmpfs to hdd/ssd${NC}"
+		echo -e "${GREEN}-backup ${RED}- ${GREEN}Backup files, if server running or not${NC}"
+		echo -e "${GREEN}-autobackup ${RED}- ${GREEN}Automaticly backup files when server running${NC}"
+		echo -e "${GREEN}-deloldbackup ${RED}- ${GREEN}Delete old backups${NC}"
+		echo -e "${GREEN}-delete_save ${RED}- ${GREEN}Delete the server's save game with the option for deleting/keeping the GameServer.ini file${NC}"
+		echo -e "${GREEN}-deloldsavefiles ${RED}- ${GREEN}Delete the server's save game (leaves the latest number files specefied in the script conf file${NC}"
+		echo -e "${GREEN}-change_branch ${RED}- ${GREEN}Changes the game branch in use by the server (public,experimental,legacy and so on)${NC}"
+		echo -e "${GREEN}-install_aliases ${RED}- ${GREEN}Installs .bashrc aliases for easy access to the server tmux session${NC}"
+		echo -e "${GREEN}-rebuild_tmux_config ${RED}- ${GREEN}Reinstalls the tmux configuration file from the script. Usefull if any tmux configuration updates occoured${NC}"
+		echo -e "${GREEN}-rebuild_services ${RED}- ${GREEN}Reinstalls the systemd services from the script. Usefull if any service updates occoured${NC}"
+		echo -e "${GREEN}-rebuild_prefix ${RED}- ${GREEN}Reinstalls the wine prefix. Usefull if any wine prefix updates occoured${NC}"
+		echo -e "${GREEN}-disable_services ${RED}- ${GREEN}Disables all services. The server and the script will not start up on boot anymore${NC}"
+		echo -e "${GREEN}-enable_services ${RED}- ${GREEN}Enables all services dependant on the configuration file of the script${NC}"
+		echo -e "${GREEN}-reload_services ${RED}- ${GREEN}Reloads all services, dependant on the configuration file${NC}"
+		echo -e "${GREEN}-update ${RED}- ${GREEN}Update the server, if the server is running it wil save it, shut it down, update it and restart it${NC}"
+		echo -e "${GREEN}-update_script ${RED}- ${GREEN}Check github for script updates and update if newer version available${NC}"
+		echo -e "${GREEN}-update_script_force ${RED}- ${GREEN}Get latest script from github and install it no matter what the version${NC}"
+		echo -e "${GREEN}-status ${RED}- ${GREEN}Display status of server${NC}"
+		echo -e "${GREEN}-install ${RED}- ${GREEN}Installs all the needed files for the script to run, the wine prefix and the game${NC}"
+		echo -e "${GREEN}-install_packages ${RED}- ${GREEN}Installs all the needed packages (Supports only Arch linux & Ubuntu 19.10 and onward)${NC}"
 		echo ""
 		echo -e "${LIGHTRED}If this is your first time running the script:${NC}"
 		echo -e "${LIGHTRED}Use the -install argument (run only this command as root) and follow the instructions${NC}"
